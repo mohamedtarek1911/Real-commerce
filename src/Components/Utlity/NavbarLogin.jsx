d0a487431d8befb9b5c7a1a71b35bdf0b2bc7417
mohamedtarek1911 <mohamedtarek7670@gmail.com>
2023-01-27 19:59:25 +0200

    second commit

--- src/Components/Utlity/NavbarLogin.jsx
+++ src/Components/Utlity/NavbarLogin.jsx
@@ -7,11 +7,11 @@ import logo from "../../Assets/images/logo.png";
 export default function NavbarLogin() {
   return (
     <>
-      <Navbar className="sticky-top" bg="dark" variant="dark" expand="sm">
+      {/* <Navbar className="sticky-top" bg="dark" variant="dark" expand="sm">
         <Container>
           <Navbar.Brand>
             <a href="/">
-              <img src={logo} alt="fkfkfhf" className="logo" />
+              <img src={logo} alt="mbscb" className="logo" />
             </a>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -31,6 +31,42 @@ export default function NavbarLogin() {
                 <p style={{ color: "white" }}>دخول</p>
               </Nav.Link>
 
+              <Nav.Link
+                href="/cart"
+                className="nav-text d-flex mt-3 justify-content-center"
+                style={{ color: "white" }}
+              >
+                <img src={cart} className="login-img" alt="sfvs" />
+                <p style={{ color: "white" }}>العربه</p>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar> */}
+
+      <Navbar className="sticky-top" bg="dark" variant="dark" expand="sm">
+        <Container>
+          <Navbar.Brand>
+            <a href="/">
+              <img src={logo} className="logo" />
+            </a>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <FormControl
+              type="search"
+              placeholder="ابحث..."
+              className="me-2 w-100 text-center"
+              aria-label="Search"
+            />
+            <Nav className="me-auto">
+              <Nav.Link
+                href="/login"
+                className="nav-text d-flex mt-3 justify-content-center"
+              >
+                <img src={login} className="login-img" alt="sfvs" />
+                <p style={{ color: "white" }}>دخول</p>
+              </Nav.Link>
               <Nav.Link
                 href="/cart"
                 className="nav-text d-flex mt-3 justify-content-center"
